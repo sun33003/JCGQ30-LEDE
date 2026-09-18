fe94a653b75b08d2e3ff7afaeaa161aebb4b7ee8
sun33003 <127114580+sun33003@users.noreply.github.com>
2026-09-18 09:27:16 +0800

    Update diy-part2-6-254.sh

--- diy-part2-6-254.sh
+++ diy-part2-6-254.sh
@@ -11,13 +11,13 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.6.254/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.6.254/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-sed -i 's/OpenWrt/LEDE-Router/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/LEDE-Router/g' package/base-files/files/bin/config_generate
 
 # Modify filename, add date prefix
-sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=AP-$(shell date +"%Y%m%d")-/1' include/image.mk
+# sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=AP-$(shell date +"%Y%m%d")-/1' include/image.mk
